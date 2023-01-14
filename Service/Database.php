33bf53b0c641019003eb7f084d7a7052680498e7
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-14 20:07:59 +0100

    [FIX] echo issu

--- Service/Database.php
+++ Service/Database.php
@@ -1,7 +1,6 @@
 <?php 
 try {
     $db = new PDO('mysql:host=localhost;dbname=movies-api;charset=utf8', 'root', 'root');
-    echo 'Connexion réussie';
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
