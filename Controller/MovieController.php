51da45f3ccb69776571c6ad0f84fa6abc279ee19
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-14 20:08:14 +0100

    [ADDON] CRUD Movies

--- Controller/MovieController.php
+++ Controller/MovieController.php
@@ -6,24 +6,76 @@ header('Content-Type: application/json');
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-switch ($requestMethod) {
-    case 'GET':
-        $movies = getAllMovies();
-        http_response_code(200);
-        echo json_encode($movies);
-        break;
-    case 'POST':
-        echo 'POST';
-        break;
-    case 'PUT':
-        echo 'PUT';
-        break;
-    case 'DELETE':
-        echo 'DELETE';
-        break;
-    default:
-        echo 'Method not allowed';
-        break;
-}
+$id = $_GET['id'] ?? null;
+    switch ($requestMethod) {
+        case 'GET':
+            if($id) {
+                $movie = getMovieById($id);
+                if($movie) {
 
+                    http_response_code(200);
+                    echo json_encode($movie);
+                }else{
+                    $error = ['code' => 404, 'message' => "Le film avec l'identifiant $id n'existe pas" ,];
+
+                    http_response_code(404);
+                    echo json_encode($error);
+                }
+            }else{
+                $movies = getAllMovies();
+                http_response_code(200);
+                echo json_encode($movies);
+            }
+            break;
+            case "POST":
+                $data = json_decode(file_get_contents('php://input'));
+                if (!isset($data->title, $data->releasedate, $data->plot, $data->runtime)) {
+                    http_response_code(400);
+                    $error = ['error' => 400, 'message' => 'Veuillez renseigner tous les champs'];
+                    echo json_encode($error);
+                } else {
+                    // Valider les données avant d'insérer
+                    $movie = createMovie($data->title, $data->releasedate, $data->plot, $data->runtime);
+                    http_response_code(201);
+                    echo json_encode($movie);
+                }
+                break;
+                
+            case "PUT":
+                $data = json_decode(file_get_contents('php://input'));
+                if (!isset($data->title, $data->releasedate, $data->plot, $data->runtime)) {
+                    http_response_code(400);
+                    $error = ['error' => 400, 'message' => 'Veuillez renseigner tous les champs'];
+                    echo json_encode($error);
+                } else {
+                    // Valider les données avant d'insérer
+                    $movie = updateMovie($id, $data->title, $data->releasedate, $data->plot, $data->runtime);
+                    http_response_code(200);
+                    echo json_encode($movie);
+                }
+                break;
+
+            case "DELETE":
+                if($id) {
+                    $movie = getMovieById($id);
+                    if($movie) {
+                        deleteMovie($id);
+
+                        // ecrire un message pour signifier que le message a bien été supprimé
+                        $message = ['code' => 200, 'message' => "Le film avec l'identifiant $id a bien été supprimé" ,];
+                        http_response_code(200);
+                        echo json_encode($message);
+
+                    }else{
+                        $error = ['code' => 404, 'message' => "Le film avec l'identifiant $id n'existe pas" ,];
+                        http_response_code(404);
+                        echo json_encode($error);
+                    }
+                }else{
+                    $error = ['code' => 400, 'message' => "Veuillez renseigner l'identifiant du film à supprimer" ,];
+                    http_response_code(400);
+                    echo json_encode($error);
+                }
+                break;
+    }
 ?>
--- Repository/MovieRepository.php
+++ Repository/MovieRepository.php
@@ -12,4 +12,69 @@ function getAllMovies():array {
     return  $getMoviesStmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function getMovieById(int $id):array {
+
+    require '../Service/Database.php';
+    
+    $sql = "SELECT * FROM movies WHERE id = :id";
+    
+    $getMovieStmt = $db->prepare($sql);
+    $getMovieStmt->bindParam(':id', $id);
+    $getMovieStmt->execute();
+
+    return  $getMovieStmt->fetchAll(PDO::FETCH_ASSOC);
+}
+    
+function createMovie($title, $releasedate, $plot, $runtime): array{
+    require '../Service/Database.php';
+
+    $sql = "INSERT INTO movies (`title`, `release_date`, `plot`, `runtime`) VALUES (:title, :releasedate, :plot, :runtime)";
+    $createMovieStmt = $db->prepare($sql);
+    $createMovieStmt->execute([
+        'title' => $title,
+        'releasedate' => $releasedate,
+        'plot' => $plot,
+        'runtime' => $runtime
+    ]);
+
+    $sql = "SELECT MAX(id) FROM movies";
+    $getMoviesCountStmt = $db->prepare($sql);
+    $getMoviesCountStmt->execute();
+
+    $lastId = $getMoviesCountStmt->fetch(PDO::FETCH_COLUMN);
+
+    return getMovieById($lastId);
+
+}
+
+function updateMovie($id, $title, $releasedate, $plot, $runtime): array{
+    require '../Service/Database.php';
+
+    $sql = "UPDATE movies SET title = :title, release_date = :releasedate, plot = :plot, runtime = :runtime WHERE id = :id";
+    $updateMovieStmt = $db->prepare($sql);
+    $updateMovieStmt->execute([
+        'id' => $id,
+        'title' => $title,
+        'releasedate' => $releasedate,
+        'plot' => $plot,
+        'runtime' => $runtime
+    ]);
+
+    return getMovieById($id);
+
+}
+
+function deleteMovie(int $id){
+    require '../Service/Database.php';
+
+    $sql = "DELETE FROM movies WHERE id = :id";
+
+    $deleteMovieStmt = $db->prepare($sql);
+
+    $deleteMovieStmt->execute([
+        'id' => $id
+    ]);
+}
+
+
 ?>
