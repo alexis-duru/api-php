92540f6cb0143d389786ac8f4017d4cd4505416a
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-15 17:59:28 +0100

    [ADDON] CRUD on genre entity

--- Controller/GenreController.php
+++ Controller/GenreController.php
@@ -28,5 +28,55 @@ switch ($requestMethod) {
             echo json_encode($genres);
         }
         break;
-   
+    case 'POST':
+        $data = json_decode(file_get_contents('php://input'));
+        if(!isset($data->name)) {
+            http_response_code(400);
+            $error = ['code' => 400, 'message' => "Le champ 'name' est obligatoire"];
+            echo json_encode($error);
+        }else{
+            $genre = createGenre($data->name);
+            http_response_code(201);
+            echo json_encode($genre);
+        }
+        break;
+    case 'PUT':
+        $data = json_decode(file_get_contents('php://input'));
+        if(!isset($data->name)) {
+            http_response_code(400);
+            $error = ['code' => 400, 'message' => "Le champ 'name' est obligatoire"];
+            echo json_encode($error);
+        }else{
+            $genre = updateGenre($id, $data->name);
+            http_response_code(200);
+            echo json_encode($genre);
+        }
+        break;
+    case 'DELETE':
+        if($id) {
+            $genre = getGenreById($id);
+            if($genre) {
+                deleteGenre($id);
+
+                $message = ['code' => 200, 'message' => "Le genre numéro $id a été supprimé" ,];
+                http_response_code(200);
+                echo json_encode($message);
+            }else{
+                $error = ['code' => 404, 'message' => "Le genre numéro $id n'existe pas" ,];
+
+                http_response_code(404);
+                echo json_encode($error);
+            }
+        }else{
+            $error = ['code' => 400, 'message' => "L'identifiant du genre est obligatoire"];
+
+            http_response_code(400);
+            echo json_encode($error);
+        }
+        break;
+    default:
+        http_response_code(405);
+        $error = ['code' => 405, 'message' => "La méthode $requestMethod n'est pas autorisée"];
+        echo json_encode($error);
+        break;
 }
--- Repository/GenreRepository.php
+++ Repository/GenreRepository.php
@@ -24,3 +24,50 @@ function getGenreById(int $id):array {
 
     return  $getGenreStmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function createGenre($name): array{
+    require '../Service/Database.php';
+
+    $sql = "INSERT INTO genres (`name`) VALUES (:name)";
+
+    $createGenreStmt = $db->prepare($sql);
+
+    $createGenreStmt->execute([
+        'name' => $name
+    ]);
+
+    $sql = "SELECT MAX(id) FROM genres";
+    $getGenresCountStmt = $db->prepare($sql);
+    $getGenresCountStmt->execute();
+
+    $lastId = $getGenresCountStmt->fetch(PDO::FETCH_COLUMN);
+
+    return getGenreById($lastId);
+}
+
+function updateGenre($id, $name): array{
+    require '../Service/Database.php';
+
+    $sql = "UPDATE genres SET name = :name WHERE id = :id";
+
+    $updateGenreStmt = $db->prepare($sql);
+
+    $updateGenreStmt->execute([
+        'id' => $id,
+        'name' => $name
+    ]);
+
+    return deleteGenre($id);
+}
+
+function deleteGenre(int $id){
+    require '../Service/Database.php';
+
+    $sql = "DELETE FROM genres WHERE id = :id";
+    
+    $deleteGenreStmt = $db->prepare($sql);
+
+    $deleteGenreStmt->execute([
+        'id' => $id
+    ]);
+}
