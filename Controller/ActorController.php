015eaa61fe5025138477c74b38c11dbe7a5c5ca6
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-15 18:15:29 +0100

    [ADDON] CRUD on actor entity

---
+++ Controller/ActorController.php
@@ -0,0 +1,85 @@
+<?php 
+
+require_once '../Repository/ActorRepository.php';
+
+header('Content-Type: application/json');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$id = $_GET['id'] ?? null;
+
+switch ($requestMethod) {
+    case 'GET':
+        if($id) {
+            $actor = getActorById($id);
+            if($actor) {
+
+                http_response_code(200);
+                echo json_encode($actor);
+            }else{
+                $error = ['code' => 404, 'message' => "L'acteur avec l'identifiant $id n'existe pas" ,];
+
+                http_response_code(404);
+                echo json_encode($error);
+            }
+        }else{
+            $actors = getAllActors();
+            http_response_code(200);
+            echo json_encode($actors);
+        }
+        break;
+    
+    case "POST":
+        $data = json_decode(file_get_contents('php://input'));
+        if (!isset($data->firstname, $data->lastname, $data->dob, $data->bio)) {
+            http_response_code(400);
+            $error = ['error' => 400, 'message' => 'Veuillez renseigner tous les champs'];
+            echo json_encode($error);
+        } else {
+            $actor = createActor($data->firstname, $data->lastname, $data->dob, $data->bio);
+            http_response_code(201);
+            echo json_encode($actor);
+        }
+        break;
+
+    case "PUT":
+        $data = json_decode(file_get_contents('php://input'));
+        if (!isset($data->firstname, $data->lastname, $data->dob, $data->bio)) {
+            http_response_code(400);
+            $error = ['error' => 400, 'message' => 'Veuillez renseigner tous les champs'];
+            echo json_encode($error);
+        } else {
+            $actor = updateActor($id, $data->firstname, $data->lastname, $data->dob, $data->bio);
+            http_response_code(200);
+            echo json_encode($actor);
+        }
+        break;
+
+    case "DELETE":
+        if($id) {
+            $actor = getActorById($id);
+            if($actor) {
+                deleteActor($id);
+                $message = ['code' => 200, 'message' => "L'acteur avec l'identifiant $id a bien été supprimé" ,];
+                http_response_code(200);
+                echo json_encode($message);
+            }else{
+                $error = ['code' => 404, 'message' => "L'acteur avec l'identifiant $id n'existe pas" ,];
+
+                http_response_code(404);
+                echo json_encode($error);
+            }
+        }else{
+            $error = ['code' => 400, 'message' => "Veuillez renseigner l'identifiant de l'acteur à supprimer" ,];
+
+            http_response_code(400);
+            echo json_encode($error);
+        }
+        break;
+
+    default:
+        http_response_code(405);
+        $error = ['error' => 405, 'message' => 'Méthode non autorisée'];
+        echo json_encode($error);
+        break;
+}
---
+++ Repository/ActorRepository.php
@@ -0,0 +1,75 @@
+<?php 
+
+function getAllActors():array {
+
+    require '../Service/Database.php';
+    
+    $sql = "SELECT * FROM actors";
+    
+    $getActorsStmt = $db->prepare($sql);
+    $getActorsStmt->execute();
+
+    return  $getActorsStmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getActorById(int $id):array {
+
+    require '../Service/Database.php';
+    
+    $sql = "SELECT * FROM actors WHERE id = :id";
+    
+    $getActorStmt = $db->prepare($sql);
+    $getActorStmt->bindParam(':id', $id);
+    $getActorStmt->execute();
+
+    return  $getActorStmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function createActor ($firstname, $lastname, $dob, $bio): array{
+    require '../Service/Database.php';
+
+    $sql = "INSERT INTO actors (`first_name`, `last_name`, `dob`, `bio`) VALUES (:firstname, :lastname, :dob, :bio)";
+    $createActorStmt = $db->prepare($sql);
+
+    $createActorStmt->execute([
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'dob' => $dob,
+        'bio' => $bio
+    ]);
+
+    $sql = "SELECT MAX(id) FROM actors";
+    $getActorsCountStmt = $db->prepare($sql);
+    $getActorsCountStmt->execute();
+
+    $lastId = $getActorsCountStmt->fetch(PDO::FETCH_COLUMN);
+
+    return getActorById($lastId);
+}
+
+function updateActor($id, $firstname, $lastname, $dob, $bio): array{
+    require '../Service/Database.php';
+
+    $sql = "UPDATE actors SET first_name = :firstname, last_name = :lastname, dob = :dob, bio = :bio WHERE id = :id";
+    $updateActorStmt = $db->prepare($sql);
+    $updateActorStmt->execute([
+        'id' => $id,
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'dob' => $dob,
+        'bio' => $bio
+    ]);
+
+    return getActorById($id);
+
+}
+
+function deleteActor(int $id){
+    require '../Service/Database.php';
+
+    $sql = "DELETE FROM actors WHERE id = :id";
+    $deleteActorStmt = $db->prepare($sql);
+    $deleteActorStmt->execute([
+        'id' => $id
+    ]);
+}
