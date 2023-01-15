a28b5aad43bd4212cc2efa716b4710b58fced007
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-15 18:00:01 +0100

    [ADDON] Crud on Review entity

---
+++ Controller/ReviewController.php
@@ -0,0 +1,81 @@
+<?php 
+
+require_once '../Repository/ReviewRepository.php';
+
+header('Content-Type: application/json');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$id = $_GET['id'] ?? null;
+
+switch ($requestMethod) {
+    case 'GET':
+        if($id) {
+            $review = getReviewById($id);
+            if($review) {
+
+                http_response_code(200);
+                echo json_encode($review);
+            }else{
+                $error = ['code' => 404, 'message' => "L'article avec l'identifiant $id n'existe pas" ,];
+
+                http_response_code(404);
+                echo json_encode($error);
+            }
+        }else{
+            $reviews = getAllReviews();
+            http_response_code(200);
+            echo json_encode($reviews);
+        }
+        break;
+    case 'POST':
+        $data = json_decode(file_get_contents('php://input'));
+        if(!isset($data->movie_id) || !isset($data->username) || !isset($data->content) || !isset($data->date)) {
+            http_response_code(400);
+            $error = ['code' => 400, 'message' => "Les champs 'movie_id , username , content , date' sont obligatoires"];
+            echo json_encode($error);
+        }else{
+            $review = createReview($data->movie_id, $data->username, $data->content, $data->date);
+            http_response_code(201);
+            echo json_encode($review);
+        }
+        break;
+    case 'PUT':
+        $data = json_decode(file_get_contents('php://input'));
+        if(!isset($data->movie_id) || !isset($data->username) || !isset($data->content) || !isset($data->date)) {
+            http_response_code(400);
+            $error = ['code' => 400, 'message' => "Les champs 'movie_id , username , content , date' sont obligatoires"];
+            echo json_encode($error);
+        }else{
+            $review = updateReview($id, $data->movie_id, $data->username, $data->content, $data->date);
+            http_response_code(200);
+            echo json_encode($review);
+        }
+        break;
+    case 'DELETE':
+        if($id) {
+            $review = getReviewById($id);
+            if($review) {
+                deleteReview($id);
+
+                $message = ['code' => 200, 'message' => "L'article avec l'identifiant $id a été supprimé" ,];
+                http_response_code(200);
+                echo json_encode($message);
+            }else{
+                $error = ['code' => 404, 'message' => "L'article avec l'identifiant $id n'existe pas" ,];
+
+                http_response_code(404);
+                echo json_encode($error);
+            }
+        }else{
+            $error = ['code' => 400, 'message' => "L'identifiant de l'article est obligatoire"];
+            http_response_code(400);
+            echo json_encode($error);
+        }
+        break;
+    default:
+        http_response_code(405);
+        $error = ['code' => 405, 'message' => "La méthode $requestMethod n'est pas autorisée"];
+        echo json_encode($error);
+        break;
+}
---
+++ Repository/ReviewRepository.php
@@ -0,0 +1,76 @@
+<?php 
+
+function getAllReviews():array {
+
+    require '../Service/Database.php';
+    
+    $sql = "SELECT * FROM reviews";
+    
+    $getReviewsStmt = $db->prepare($sql);
+    $getReviewsStmt->execute();
+
+    return  $getReviewsStmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getReviewById(int $id):array {
+
+    require '../Service/Database.php';
+    
+    $sql = "SELECT * FROM reviews WHERE id = :id";
+    
+    $getReviewsStmt = $db->prepare($sql);
+    $getReviewsStmt->bindParam(':id', $id);
+    $getReviewsStmt->execute();
+
+    return  $getReviewsStmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function createReview($movie_id, $username, $content, $date): array{
+    require '../Service/Database.php';
+
+    $sql = "INSERT INTO reviews (`movie_id`, `username`, `content`, `date`) VALUES (:movie_id, :username, :content, :date)";
+    $createReviewStmt = $db->prepare($sql);
+    $createReviewStmt->execute([
+        'movie_id' => $movie_id,
+        'username' => $username,
+        'content' => $content,
+        'date' => $date
+    ]);
+
+    $sql = "SELECT MAX(id) FROM reviews";
+    $getReviewsCountStmt = $db->prepare($sql);
+    $getReviewsCountStmt->execute();
+
+    $lastId = $getReviewsCountStmt->fetch(PDO::FETCH_COLUMN);
+
+    return getReviewById($lastId);
+}
+
+function updateReview($id, $movie_id, $username, $content, $date): array{
+    require '../Service/Database.php';
+
+    $sql = "UPDATE reviews SET movie_id = :movie_id, username = :username, content = :content, date = :date WHERE id = :id";
+    $updateReviewStmt = $db->prepare($sql);
+    $updateReviewStmt->execute([
+        'id' => $id,
+        'movie_id' => $movie_id,
+        'username' => $username,
+        'content' => $content,
+        'date' => $date
+    ]);
+
+    return getReviewById($id);
+
+}
+
+function deleteReview(int $id){
+    require '../Service/Database.php';
+
+    $sql = "DELETE FROM reviews WHERE id = :id";
+    
+    $deleteReviewStmt = $db->prepare($sql);
+
+    $deleteReviewStmt->execute([
+        'id' => $id
+    ]);
+}
