73ed71a44f78b0960244610735fa7b380a251467
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-19 13:50:25 +0100

    [FIX] ERRMODE_EXCEPTION on database file

--- Service/Database.php
+++ Service/Database.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=movies-api;charset=utf8', 'root', 'root');
+    $db = new PDO('mysql:host=localhost;dbname=movies-api;charset=utf8', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
