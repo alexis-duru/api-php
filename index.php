dd8090589a96c6ae4dae6473cd7a23fb4886b929
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-01-26 10:31:16 +0100

    [CSS] FRONT OF API

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API MOVIES - PHP</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>API MOVIES</h1>
+    <img src="assets/img/php-logo-white.svg"/>
+    <ul class="menu">
+        <li>
+            <a href="index.php">Accueil</a>
+        </li>
+        <li>
+            <a href="/api/movies">Movies</a>
+        </li>
+        <li>
+            <a href="/api/directors">Directors</a>
+        </li>
+        <li>
+            <a href="/api/actors">Actors</a>
+        </li>
+        <li>
+            <a href="/api/reviews">Reviews</a>
+        </li>
+        <li>
+            <a href="/api/genres">Genres</a>
+        </li>
+    </ul>
+</body>
+</html>
